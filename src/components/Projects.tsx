a01b6bd465b5dc13a8535a9c5f024f98a41fe954
Yuriy Zhvirblis <yuriyzdev@gmail.com>
2024-07-23 20:30:49 +0300

    projects section with mockup

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,58 +1,91 @@
-import { useState } from 'react'
-
-const projectsData = [
-    {
-        category: 'Web Development',
-        projects: [
-            {
-                title: 'Project 1',
-                description: 'Description of project 1',
-                link: 'https://github.com/YZvirblis/project1'
-            },
-            {
-                title: 'Project 2',
-                description: 'Description of project 2',
-                link: 'https://github.com/YZvirblis/project2'
-            }
-        ]
-    },
-    {
-        category: 'Game Development',
-        projects: [
-            {
-                title: 'Game 1',
-                description: 'Description of game 1',
-                link: 'https://github.com/YZvirblis/game1'
-            }
-        ]
-    }
-]
+import { useEffect, useState, useRef } from 'react'
+import ProjectsData from '../data/projects'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
 const Projects = () => {
-    const [expandedCategory, setExpandedCategory] = useState<string | null>(null)
+    const [rotation, setRotation] = useState(0)
+    const [currentCategoryIndex, setCurrentCategoryIndex] = useState(0)
+    const [isVisible, setIsVisible] = useState(false)
+    const projectsRef = useRef(null)
+
+    const handleLeftClick = () => {
+        const newRotation = rotation - (360 / ProjectsData.length)
+        setRotation(newRotation)
+        updateCurrentCategory(newRotation)
+    }
+
+    const handleRightClick = () => {
+        const newRotation = rotation + (360 / ProjectsData.length)
+        setRotation(newRotation)
+        updateCurrentCategory(newRotation)
+    }
+
+    const updateCurrentCategory = (newRotation: number) => {
+        const index = Math.round((newRotation / (360 / ProjectsData.length)) % ProjectsData.length)
+        setCurrentCategoryIndex((index + ProjectsData.length) % ProjectsData.length) // Ensure the index is always positive
+    }
+
+    useEffect(() => {
+        updateCurrentCategory(rotation)
+    }, [rotation])
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    setIsVisible(entry.isIntersecting)
+                })
+            },
+            { threshold: 0.1 }
+        )
+
+        if (projectsRef.current) {
+            observer.observe(projectsRef.current)
+        }
+
+        return () => {
+            if (projectsRef.current) {
+                observer.unobserve(projectsRef.current)
+            }
+        }
+    }, [])
 
     return (
-        <section id="projects" className="container mx-auto p-4">
-            <h2 className="text-3xl font-bold mb-4">Projects</h2>
-            { projectsData.map((categoryData, index) => (
-                <div key={ index }>
-                    <h3
-                        className="text-2xl font-semibold cursor-pointer"
-                        onClick={ () => setExpandedCategory(expandedCategory === categoryData.category ? null : categoryData.category) }
-                    >
-                        { categoryData.category }
-                    </h3>
-                    <div className={ expandedCategory === categoryData.category ? 'block' : 'hidden' }>
-                        { categoryData.projects.map((project, idx) => (
-                            <div key={ idx } className="mb-4">
-                                <h4 className="text-xl font-semibold">{ project.title }</h4>
-                                <p>{ project.description }</p>
-                                <a href={ project.link } className="text-blue-500">View Project</a>
-                            </div>
-                        )) }
-                    </div>
+        <section id="projects" className="container mx-auto p-4 relative flex flex-col" ref={ projectsRef }>
+            <button onClick={ handleLeftClick } className={ `carousel-button left-4 ${isVisible ? 'show' : ''}` }>
+                <FontAwesomeIcon icon={ faChevronLeft } />
+            </button>
+            <button onClick={ handleRightClick } className={ `carousel-button right-4 ${isVisible ? 'show' : ''}` }>
+                <FontAwesomeIcon icon={ faChevronRight } />
+            </button>
+            <div className='projects-banner'>
+                <div className='projects-slider' style={ { '--projects-quantity': ProjectsData.length, transform: `perspective(1000px) rotateY(${rotation}deg)` } as React.CSSProperties }>
+                    { ProjectsData.map((categoryData, index) => (
+                        <div key={ index } className='projects-item mt-16' style={ { '--projects-item-position': index } as React.CSSProperties }>
+                            <img src={ categoryData.image } alt={ categoryData.category } />
+                        </div>
+                    )) }
                 </div>
-            )) }
+            </div>
+            <div className="projects-details mt-8 p-4 bg-gray-100 rounded-lg shadow-lg">
+                <h2 className="text-2xl font-bold mb-4">{ ProjectsData[currentCategoryIndex].category }</h2>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    { ProjectsData[currentCategoryIndex].projects.map((project, idx) => (
+                        <div key={ idx } className="project-item bg-white p-4 rounded-lg shadow-md">
+                            <h3 className="text-xl font-semibold mb-2">{ project.title }</h3>
+                            <img src={ project.image ? project.image : ProjectsData[currentCategoryIndex].image } alt={ `${project.title} image` } />
+                            <p className="text-gray-700 mb-2">{ project.description }</p>
+                            <div className="flex flex-wrap">
+                                { project.technologies.map((tech, techIdx) => (
+                                    <img key={ techIdx } src={ tech.logo } alt={ tech.name } className="h-6 w-6 mr-2 mb-2" />
+                                )) }
+                            </div>
+                            <a href={ project.link } target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">View Project</a>
+                        </div>
+                    )) }
+                </div>
+            </div>
         </section>
     )
 }
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -17,6 +17,21 @@ const ProjectsData: Category[] = [
         image: "/images/js-logo.png",
         link: "https://github.com/YZvirblis/project1",
       },
+      {
+        title: "Project 1 melel dfgsdg vsdvrg",
+        description:
+          "lorem ipsum dolor sit am sdfsd nljsd oijsfghsdfhafdhad aefhadfha esrheadfh adfhadfhstd jsrfrkja fgjsthj sfgbz dfgb fstu sfghv dfh sfgjh dfga dfhsgf hfh sfgj sgfj sfghj syfkj sfghg sfgj sfgj sfgj sfgj sgfj sfgjfoisdj oisjdfosidjf oijdfoisdjf oijsdoifsdf vkrjgh sdhoughwoe sdnowiej ",
+        technologies: getTechnologies([
+          "HTML",
+          "CSS",
+          "React",
+          "AWS",
+          "Serverless",
+          "MongoDB",
+        ]),
+        image: "",
+        link: "https://github.com/YZvirblis/project1",
+      },
     ],
   },
   {
@@ -28,6 +43,19 @@ const ProjectsData: Category[] = [
         description: "Description of project 1",
         technologies: getTechnologies(["HTML", "CSS"]),
         image: "/images/js-logo.png",
+        link: "https://github.com/YZvirblis/project1",
+      },
+    ],
+  },
+  {
+    category: "React",
+    image: "/images/react-logo.png",
+    projects: [
+      {
+        title: "Project 1",
+        description: "Description of project 1",
+        technologies: getTechnologies(["HTML", "CSS"]),
+        image: "",
         link: "https://github.com/YZvirblis/project1",
       },
     ],
